274cd7c6da4a81aa9b2fcf0354912d4473b06327
Kevyn <dosreiskevyn@gmail.com>
2025-03-11 12:33:31 -0300

    exercicios de const e let

--- exercicios/exercicio 05.js
+++ exercicios/exercicio 05.js
@@ -1,1 +1,13 @@
+//EXERCICIO DE CONST E LET
 
+const nome = 'João';
+const sobrenome = 'Silva';
+const idade = 27;
+const peso = 83;
+const altura = 1.70;
+
+let imc = peso / (altura * altura);
+let anoNascimento = 2021 - idade;
+
+console.log(`${nome} ${sobrenome} nasceu em ${anoNascimento}, pesa ${peso} kg`);
+console.log(`tem ${altura} de altura e seu IMC é de ${imc}`);
