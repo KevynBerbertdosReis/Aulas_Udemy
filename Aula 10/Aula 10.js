2dbc0740586000c75468facf42927964c166f6a5
Kevyn <dosreiskevyn@gmail.com>
2025-03-17 22:35:01 -0300

    Operadores de adição e incremento

---
+++ Aula 10/Aula 10.js
@@ -0,0 +1,51 @@
+//operadores aritméticos ( + - * / % **)
+
+//(SOMA)
+//num1 = 4;
+//num2 = 30;
+//console.log(num1 + num2);
+
+//(SUBTRAÇÃO)
+//num1 = 8;
+//num2 = 20;
+//console.log(num1 - num2);
+
+//(MULTIPLICAÇÃO)
+//num1 = 20;
+//num2 = 50;
+//console.log(num1 * num2);
+
+//(DIVISÃO)
+//num1 = 100;
+//num2 = 10;
+//console.log(num1 / num2);
+
+//(RESTO DA DIVISÃO)
+//num1 = 10;
+//num2 = 5;
+//console.log(num1 % num2);
+
+//(EXPONENCIAÇÃO)
+//num1 = 4;
+//num2 = 10;
+//console.log(num1 ** num2);
+
+
+//Incremento ++;
+//Decremento --;
+
+//let numero1 = 1;
+//numero1 ++;
+//numero1 ++;
+//numero1 ++;
+//numero1 ++;
+//numero1 ++;
+//console.log(numero1);
+
+//NaN - Not a Number
+//let num1 = 10;
+//let num2 = 'Paulo';
+//console.log(num1 * num2);
+
+//parseInt() - Converte para inteiro
+//parseFloat() - Converte para decimais
