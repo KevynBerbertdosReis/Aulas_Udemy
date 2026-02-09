5d6ae8255a683bfe5e1be6cae7eb1ebf3231ee7d
Kevyn <dosreiskevyn@gmail.com>
2026-02-09 17:46:52 -0300

    Criando um Form

--- Aula 21/index.js
+++ Aula 21/index.js
@@ -4,10 +4,10 @@
 ! -> NOT -> NÃO -> ele nega uma expressão, invertendo o seu valor
 */
 
-//const expressaoAnd = true && true && false;
-//const expressaoOr =  false || false || false || false;
+const expressaoAnd = true && true && false; 
+const expressaoOr =  false || false || false || false;
 
-//const user = 'Luiz';
-//const password = '45689';
-//const validation = user === 'Luiz' && password === '45659';
-//console.log(!false) 
+const user = 'Luiz';
+const password = '45689';
+const validation = user === 'Luiz' && password === '45659';
+console.log(!false) 
